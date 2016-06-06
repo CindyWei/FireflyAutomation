3b8ef27873f6329a8d208d3d06e2a951fdaf99fc
weiyaxin <wb-weiyaxin@alibaba-inc.com>
2016-06-06 14:23:01 +0800

    java selenium

---
+++ trunk/src/practicetwo/launch/TestBrowsers.java
@@ -0,0 +1,40 @@
+package practicetwo.launch;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class TestBrowsers {
+	
+	private WebDriver driver;
+	
+	
+	@BeforeClass
+	public void beforeClass(){
+		Browsers browser = new Browsers(BrowsersType.ie);
+		driver = browser.driver;
+	}
+
+	@Test
+	public void start(){
+		
+		
+		driver.get("http://www.126.com");
+		driver.findElement(By.xpath("//input[@id='idInput']")).clear();
+		driver.findElement(By.xpath("//input[@id='idInput']")).sendKeys("FireflyAutomation");
+		
+		driver.findElement(By.xpath("//input[@id='pwdInput']")).sendKeys("Firefly");
+		driver.findElement(By.xpath("//input[@id='pwdInput']")).submit();
+		
+	}
+	
+	@AfterClass
+	public void quit(){
+		driver.quit();
+	}
+	
+	
+}
